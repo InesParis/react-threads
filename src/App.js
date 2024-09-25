1b73e1c326f453ad334676c5675fbd1a7b14c4c4
InesParis <inesparisarranz@gmail.com>
2024-09-25 18:26:28 +0200

    working on problem with db.json file
    fixing backend problem

--- src/App.js
+++ src/App.js
@@ -2,27 +2,17 @@ import { useState, useEffect } from "react";
 import Nav from "./components/Nav";
 import Header from "./components/Header";
 import Feed from "./components/Feed";
-import PopUp from "./components/PopUp";
+import userData from "./db.json";
+
 const App = () => {
   const [user, setUser] = useState(null);
 
   const userId = "cf0fd3bd-8f70-4a13-b056-0b63b0e99c5a";
 
-  const getUser = async () => {
-    try {
-      const response = await fetch(
-        `http://localhost:3000/users?user_uuid=${userId}`
-      );
-      const data = await response.json();
-      setUser(data[0]);
-    } catch (error) {
-      console.error(error);
-    }
-  };
   useEffect(() => {
-    getUser();
-  }, []);
-  console.log(user);
+    const foundUser = userData.users.find((user) => user.user_uuid === userId);
+    setUser(foundUser);
+  });
 
   return (
     <>
--- src/components/Header.js
+++ src/components/Header.js
@@ -15,10 +15,7 @@ const Header = ({ user }) => {
       </div>
       <p>{user.bio}</p>
       <div className="sub-info-container">
-        <p className="sub-text">
-          {user.followers.length}
-          followers · <a href="">link</a>
-        </p>
+        <p className="sub-text">{user.followers.length} followers ·</p>
       </div>
 
       <button
